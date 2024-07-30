6b7e803828553d8ca7cc49c417bcafde9d0580bd
amnepi@yandex.ru <amnepi@yandex.ru>
2024-07-30 15:48:00 +0300

    first commit

---
+++ fsto/src/main/java/fsto/fsto/FstoApplication.java
@@ -0,0 +1,13 @@
+package fsto.fsto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FstoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FstoApplication.class, args);
+	}
+
+}
---
+++ fsto/src/test/java/fsto/fsto/FstoApplicationTests.java
@@ -0,0 +1,13 @@
+package fsto.fsto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FstoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
